93fd5432ee977518c1621f03cca12a808235ecf1
hissinger <loje75@gmail.com>
2018-12-19 00:07:43 +0900

    Add files via upload

---
+++ snap-camera-remover.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+currentHome=/Users/hissinger
+currentUser=`echo $currentHome | cut -d'/' -f 3`
+currentUid=`id -u $currentUser`
+
+launchctl asuser $currentUid launchctl unload -w $currentHome/Library/LaunchAgents/com.snap.AssistantService.plist
+rm $currentHome/Library/LaunchAgents/com.snap.AssistantService.plist
+launchctl asuser $currentUid launchctl remove com.snap.AssistantService
+
+rm -rf $currentHome/'Library/Preferences/Snap/Snap Camera'
+rm -rf $currentHome/'Library/Caches/Snap/Snap Camera'
+
+rm -rf /Library/CoreMediaIO/Plug-Ins/DAL/SnapCamera.plugin
+
+launchctl unload -w /Library/LaunchDaemons/com.snap.SnapCameraRemover.plist
+rm /Library/LaunchDaemons/com.snap.SnapCameraRemover.plist
+launchctl remove com.snap.SnapCameraRemover
